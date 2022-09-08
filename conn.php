1ac25178e2a70c7a6a1712c771d3f669ac64b113
OsamhAlahdl <samalahdl11@gmail.com>
2022-09-08 13:39:34 +0300

    add css
    add bootstrab

--- conn.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Hello To Mystrios Team";
-echo "Hello To Mystrios java";
-?>
--- index.php
+++ index.php
@@ -1,3 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <!-- Default form login -->
+<form class="text-center border border-light p-5" action="#!">
+
+<p class="h4 mb-4">Sign in</p>
+
+<!-- Email -->
+<input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+<!-- Password -->
+<input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
+
+<div class="d-flex justify-content-around">
+    <div>
+        <!-- Remember me -->
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+            <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+        </div>
+    </div>
+    <div>
+        <!-- Forgot password -->
+        <a href="">Forgot password?</a>
+    </div>
+</div>
+
+<!-- Sign in button -->
+<button class="btn btn-info btn-block my-4" type="submit">Sign in</button>
+
+<!-- Register -->
+<p>Not a member?
+    <a href="">Register</a>
+</p>
+
+<!-- Social login -->
+<p>or sign in with:</p>
+
+<a href="#" class="mx-2" role="button"><i class="fab fa-facebook-f light-blue-text"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fab fa-twitter light-blue-text"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fab fa-linkedin-in light-blue-text"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fab fa-github light-blue-text"></i></a>
+
+</form>
+<!-- Default form login -->
+</body>
+</html>
+
+
+
 <?php
 echo "My syshar";
 https://mdbootstrap.com/
