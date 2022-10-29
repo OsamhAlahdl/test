595c0a769e5f6a4180b276ac474fe29223efdd1e
MOHAMMEDMODjr <mohammedmodabish1@gmail.com>
2022-10-29 17:12:41 +0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,10 +1,16 @@
-<?php
+from logging import root
+from tkinter import *
 
-//$host="sql113.epizy.com";
-//$user="epiz_32570108";
-//$passsword="fE1FCHLjzWpGU";
-//$db="epiz_32570108_team";
- //$conn = mysqli_connect($host,$user,$passsword,$db);
- // $conn = mysqli_connect("localhost","root","","test");
+def mainWindow():
+   root = Tk()
+   root.geometry("400x400")
+   root.tk_setPalette('#C8BBBE')
+   fLabel = Label(root, text=" واجهة التسجيل",font=("courier bold" , 40)).place(x=60, y=70)
 
-?>
+
+   signupButt = Button(root, text="تسجيل الدخول", padx=0, pady=0, fg="#F0F8FF", bg="#5F9EA0",font=("courier bold",20),
+                       command=lambda:[root.destroy() , SignUp()] ).place(x=125, y=150)
+   signinButt = Button(root, text="تسجل جديد ", padx=10, pady=0, fg="#F0F8FF", bg="#F08080",font=("courier bold",20),
+                       command=lambda: [root.destroy(), SignIn()]).place(x=125, y=220)
+   root.title("واجهة تسجيل الدخول")
+   root.mainloop()
